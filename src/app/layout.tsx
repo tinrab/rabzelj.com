0ecb9b453b78b993db9a5f6c61f9aa2172f2ca94
Tin Rabzelj <tin@flinect.com>
2024-07-17 23:23:21 +0200

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,34 @@
-import type { Metadata } from 'next';
-import './globals.css';
-import { cn } from '@/lib/utils';
-import { Roboto as FontSans } from 'next/font/google';
+import type { Metadata } from "next";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import { Roboto as FontSans } from "next/font/google";
 
 const fontSans = FontSans({
-  weight: ['300', '400', '500', '700'],
-  subsets: ['latin'],
-  variable: '--font-sans',
+	weight: ["300", "400", "500", "700"],
+	subsets: ["latin"],
+	variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
-  title: 'Tin Rabzelj',
-  description: "Tin Rabzelj's personal website",
+	title: "Tin Rabzelj",
+	description: "Tin Rabzelj's personal website",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body
-        className={cn(
-          'min-h-screen bg-background font-sans antialiased',
-          fontSans.variable,
-        )}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body
+				className={cn(
+					"min-h-screen bg-background font-sans antialiased",
+					fontSans.variable,
+				)}
+			>
+				{children}
+			</body>
+		</html>
+	);
 }
