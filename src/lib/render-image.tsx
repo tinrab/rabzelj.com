4ef8642291eccf5ffb72e42e5744ca752c7853f4
Tin Rabzelj <tin@flinect.com>
2026-02-23 06:19:28 +0100

    Fix nitro bundle

--- src/lib/render-image.tsx
+++ src/lib/render-image.tsx
@@ -1,13 +1,36 @@
-// import { Resvg } from "@resvg/resvg-js";
 import type React from "react";
 
 import fontSans from "@fontsource/roboto/files/roboto-latin-400-normal.woff?arraybuffer";
+import { Resvg, initWasm } from "@resvg/resvg-wasm";
+// import resvgWasm from "@resvg/resvg-wasm/index_bg.wasm?url";
+import resvgWasm from "@resvg/resvg-wasm/index_bg.wasm?arraybuffer";
+import fs from "node:fs/promises";
+import path from "node:path";
 import satori from "satori";
+
+// let resvgInitialized = false;
+// async function initResvg(): Promise<void> {
+//   if (!resvgInitialized) {
+//     await initWasm(
+//       fs.readFile(path.join(process.cwd(), resvgWasm)).then((buffer) => new Uint8Array(buffer)),
+//     );
+//     resvgInitialized = true;
+//   }
+// }
+
+let resvgInitialized = false;
+async function initResvg(): Promise<void> {
+  if (!resvgInitialized) {
+    await initWasm(resvgWasm);
+    // await resvgWasm();
+    resvgInitialized = true;
+  }
+}
 
 export async function renderImage(
   node: React.ReactNode,
   { width, height }: { width: number; height: number },
-): Promise<Buffer> {
+): Promise<Uint8Array> {
   const svg = await satori(node, {
     width,
     height,
@@ -20,8 +43,7 @@ export async function renderImage(
     ],
   });
 
-  const { Resvg } = await import("@resvg/resvg-js");
-
+  await initResvg();
   const resvg = new Resvg(svg);
   const pngData = resvg.render();
   return pngData.asPng();
